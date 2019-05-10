d27aa5aeff3007c7554798126ff2b03e851aa229
Jannou <jannou59@gmail.com>
2019-05-10 17:24:29 +0200

    feat(register): add register

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,34 @@
-import {MatButtonModule, MatCardModule, MatCheckboxModule} from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule
+} from '@angular/material';
 import {NgModule} from "@angular/core";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatCheckboxModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
   ],
   exports: [
     MatButtonModule,
     MatCheckboxModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
   ],
 })
 export class MaterialModule { }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import {environment} from "../../environments/environment";
+import {User} from "../models/user.model";
+
+@Injectable({providedIn: 'root'})
+export class UserService {
+  constructor(private http: HttpClient) {
+  }
+
+  getAll() {
+    return this.http.get<User[]>(`${environment.apiBaseUrl}/user`);
+  }
+
+  register(register) {
+    this.http.post<User>(`${environment.apiBaseUrl}/auth/register`, register).subscribe(user => {
+      return user;
+    });
+  }
+}
