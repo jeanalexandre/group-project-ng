4ddd9e2efb5fb7d004260e4378ca5c6b5cde489e
Jannou <jannou59@gmail.com>
2019-05-10 17:24:29 +0200

    feat(register): add register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AppComponent, pathMatch: 'full'},
+  {path: 'login', loadChildren: './login/login.module#LoginModule'},
+  {path: 'register', loadChildren: './register/register.module#RegisterModule'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
